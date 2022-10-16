31b4002408f3d3df8e4072006e2989ae81c5c7fd
Noaman <noamanahmd@gmail.com>
2022-10-16 16:42:45 +0500

    Added Intial Files

---
+++ installers/yt-dlp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+if [ ! -d "/opt/lemp/" ]
+then
+  echo "LEMP installer not installed. Aborting!"
+  exit 1;
+fi
+
+template_path="/opt/lemp/templates"
+source /opt/lemp/includes/helpers.sh
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
